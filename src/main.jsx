75188f8413301305615062157017eac4ba026950
SarikaRAJPURE <sprajpure@gmail.com>
2023-12-22 20:29:37 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import Products from './Products.jsx';
+import Product from './Product.jsx';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/products',
+        element: <Products />,
+    },
+    {
+        path: '/products/:productId',
+        element: <Product />,
+    },
+]);
+
+const queryClient = new QueryClient();
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    //<React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    </QueryClientProvider>
+    //</React.StrictMode>
+);
